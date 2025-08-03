52418819cc263cd7b6c35de06e17571072ce6cec
Jido Mad <smileboy1422@gmail.com>
2025-08-03 14:54:54 +0100

    replaced 3 with 0 (im just trying things on github)

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="flex items-center justify-between p-2 font-roboto-bold bg-[#693F26] text-white">
       <MdListAlt className=" text-2xl" />
-      <span>0/3 To Dos Completed</span>
+      <span>0/0 To Dos Completed</span>
     </div>
   );
 }
