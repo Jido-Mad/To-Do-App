e366ba12a0e7725c43b5f2d2ef42f6e20c6f3d9e
Jido Mad <smileboy1422@gmail.com>
2025-08-16 16:15:40 +0100

    Added the Confirm Changes Logic, the Undo Changes Logic, added Aria Lbels and titles for buttons, onKeyDown methods, and tried to eliminate some repition on styling(still in progess)

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -3,25 +3,46 @@ import { BsTrashFill } from "react-icons/bs";
 import { useState, useEffect, useRef } from "react";
 
 function Task({ name, taskDel }) {
+  const [taskName, setTaskName] = useState(name);
   const [inputValue, setInputValue] = useState(name);
   const [editor, setEditor] = useState(false);
   const inputRef = useRef(null);
+  const liClasses =
+    "flex items-center justify-between p-3 border-l-8 border-[#976f47] shadow-md rounded-sm w-full h-15";
 
-  function handleEditor() {
-    setEditor((prev) => !prev);
+  function editTask() {
+    setEditor(true);
   }
-  +useEffect(() => {
+
+  /* Logic to select and focus the text when pressing the edit button */
+  useEffect(() => {
     if (editor && inputRef.current) {
       inputRef.current.focus();
       inputRef.current.select();
     }
   }, [editor]);
 
+  /* Confirm Changes Function */
+  function confirmChanges() {
+    setEditor(false);
+    setTaskName(inputValue);
+  }
+
+  /* Undo Changes Function */
+  function undoChanges() {
+    setInputValue(taskName);
+    setEditor(false);
+  }
+
   return (
     <>
       {editor ? (
-        <li className="flex items-center justify-between p-3 border-l-8 border-[#976f47] shadow-md rounded-sm w-full h-15">
+        <li className={liClasses}>
           <input
+            onKeyDown={(e) => {
+              if (e.key === "Enter") return confirmChanges();
+              if (e.key === "Escape") return undoChanges();
+            }}
             ref={inputRef}
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
@@ -29,31 +50,47 @@ function Task({ name, taskDel }) {
             type="text"
           />
           <div className="flex gap-x-4 ">
-            {/* Edit Button */}
-            <button className="text-[#976f47] text-sm hover:text-[#693F26] cursor-pointer transition-all ease-in-out">
+            {/* Confirm Changes Button */}
+            <button
+              onClick={confirmChanges}
+              title="Confirm Changes"
+              aria-label="Confirm Changes"
+              className="text-[#976f47] text-sm hover:text-[#693F26] cursor-pointer transition-colors ease-in-out"
+            >
               Confirm
             </button>
-            {/* Remove Button */}
-            <button className=" text-sm flex items-center justify-center text-white bg-[#976f47] w-12 h-8 rounded-md cursor-pointer hover:bg-[#693F26] transition-all ease-in-out">
+            {/* Undo Changes Button */}
+            <button
+              onClick={undoChanges}
+              title="Undo Changes"
+              aria-label="Undo Changes"
+              className="text-sm flex items-center justify-center text-white bg-[#976f47] w-12 h-8 rounded-md cursor-pointer hover:bg-[#693F26] transition-colors ease-in-out"
+            >
               Undo
             </button>
           </div>
         </li>
       ) : (
-        <li className="flex items-center justify-between p-3 border-l-8 border-[#976f47] shadow-md rounded-sm w-full h-15">
-          <p className="font-roboto-bold overflow-auto max-w-45 p-2">{name}</p>
+        <li className={liClasses}>
+          <p className="font-roboto-bold overflow-auto max-w-45 p-2">
+            {taskName}
+          </p>
           <div className="flex gap-x-4 ">
             {/* Edit Button */}
             <button
-              onClick={handleEditor}
-              className="text-[#976f47] text-2xl hover:text-[#693F26] cursor-pointer transition-all ease-in-out"
+              onClick={editTask}
+              title="Edit Task"
+              aria-label="Edit Task"
+              className="text-[#976f47] text-2xl hover:text-[#693F26] cursor-pointer transition-colors ease-in-out"
             >
               <AiFillEdit />
             </button>
             {/* Remove Button */}
             <button
               onClick={taskDel}
-              className="flex items-center justify-center text-white bg-[#976f47] w-12 h-8 rounded-md cursor-pointer hover:bg-[#693F26] transition-all ease-in-out"
+              title="Delete Task"
+              aria-label="Delete Task"
+              className="flex items-center justify-center text-white bg-[#976f47] w-12 h-8 rounded-md cursor-pointer hover:bg-[#693F26] transition-colors ease-in-out"
             >
               <BsTrashFill />
             </button>
